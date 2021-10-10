c40f796b4d6862acee5e629dffde5e2f6e752d75
gkuznetsov <g.kuznetsov@siams.com>
2021-10-10 13:36:57 +0500

    step-2: create PersonListViewModel

--- app/src/main/java/com/study/recycler/fragment/PersonListFragment.kt
+++ app/src/main/java/com/study/recycler/fragment/PersonListFragment.kt
@@ -19,36 +19,6 @@ class PersonListFragment : Fragment(R.layout.fragment_user_list) {
     private var _binding: FragmentUserListBinding? = null
     private val binding get() = _binding!!
 
-    private var persons = listOf(
-        Person.Developer(
-            id = UUID.randomUUID().toString(),
-            name = "John Smith",
-            avatarLink = "https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_1280.png",
-            age = 32,
-            programmingLanguage = "Java"
-        ),
-        Person.User(
-            id = UUID.randomUUID().toString(),
-            name = "Kevin Costner",
-            avatarLink = "https://png.pngtree.com/png-clipart/20190922/original/pngtree-business-male-user-avatar-vector-png-image_4774078.jpg",
-            age = 29,
-        ),
-        Person.User(
-            id = UUID.randomUUID().toString(),
-            name = "Jane Blane",
-            avatarLink = "https://png.pngtree.com/png-clipart/20190924/original/pngtree-female-user-avatars-flat-style-women-profession-vector-png-image_4822944.jpg",
-            age = 25,
-
-        ),
-        Person.Developer(
-            id = UUID.randomUUID().toString(),
-            name = "Helen Snow",
-            avatarLink = "https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_1280.png",
-            age = 40,
-            programmingLanguage = "Kotlin"
-        ),
-    )
-
     private var personAdapter: PersonAdapter by AutoClearedValue(this)
 
     override fun onCreateView(
@@ -86,18 +56,12 @@ class PersonListFragment : Fragment(R.layout.fragment_user_list) {
     }
 
     private fun deletePerson(position: Int) {
-        persons = persons.filterIndexed{ index, user -> index != position}
+
         personAdapter.items = persons
     }
 
     private fun addPerson() {
-        val newUser = persons.random().let {
-            when(it) {
-                is Person.Developer -> it.copy(id = UUID.randomUUID().toString())
-                is Person.User -> it.copy(id = UUID.randomUUID().toString())
-            }
-        }
-        persons = listOf(newUser) + persons
+
         personAdapter.items = persons
         binding.userList.scrollToPosition(0)
     }
