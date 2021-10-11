27d226141f259b1527da0529d7e7d53c856a3c71
gkuznetsov <g.kuznetsov@siams.com>
2021-10-11 17:48:27 +0500

    step-11: add NavigationGraph using

--- app/src/main/java/com/study/recycler/MainActivity.kt
+++ app/src/main/java/com/study/recycler/MainActivity.kt
@@ -9,10 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, PersonListFragment())
-            .commit()
     }
 
 }
